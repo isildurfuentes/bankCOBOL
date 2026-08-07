0a912fb9aa9d27059e47b981c8eed38db1022981
Isidro Fuentes <isidrofuenteshermoso@gmail.com>
2026-08-07 14:59:01 +0200

    fix: Actualizada estructura tipos de movimientos

--- MySQL/ddl/tiposmovimiento.sql
+++ MySQL/ddl/tiposmovimiento.sql
@@ -2,7 +2,7 @@ CREATE TABLE `tiposMovimiento` (
   `id` int unsigned NOT NULL,
   `descripcion` varchar(45) DEFAULT NULL,
   `signo` varchar(1) NOT NULL,
-  `importeMinimo` decimal(10,0) DEFAULT NULL,
+  `importeMinimo` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
+++ fuentes/tipos/tiposMovimiento_tipos.cpy
@@ -0,0 +1,6 @@
+       01 REG-TIPO-MOVIMIENTO.
+        03 ID-TIPO-MOV       PIC 99.
+        03 DESC-TIPO-MOV     PIC X(45).
+        03 SIGNO-TIPO-MOV    PIC X.
+        03 IMPORTE-MINIMO    PIC S9(10)V99.
+        
